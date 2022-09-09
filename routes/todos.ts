d481fdfa319bdf3703ea0db14b94a3b3f93c0988
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-09-09 17:36:24 +0530

    feat: Adds crud routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -14,8 +14,29 @@ router.post("/todo", (req, res, next) => {
     id: new Date().toISOString(),
     text: req.body.text,
   };
-
   todos.push(newTodo);
+
+  res
+    .status(201)
+    .json({ message: "Created the todo.", todo: newTodo, todos: todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res.status(200).json({ message: "Updated!", todos: todos });
+  }
+
+  res.status(404).json({ message: "Could not find todo item for this id." });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  todos = todos.filter((todoItem) => todoItem.id !== tid);
+  res.status(200).json({ message: "Deleted todo.", todos: todos });
 });
 
 export default router;
